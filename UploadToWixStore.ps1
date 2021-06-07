d3efb477186465fe8d7f22cc2c515b89001652d5
J. Alexander <xandermoorman@gmail.com>
2021-06-07 15:56:08 -0600

    Update UploadToWixStore.ps1

--- UploadToWixStore.ps1
+++ UploadToWixStore.ps1
@@ -5,14 +5,14 @@ $Credential = Get-Credential
 ## Get initial Cookies
 
 $wc = New-Object System.Net.WebClient
-$wc.Headers.Add("User-Agent", "User-Agent:Mozilla/4.0 (compatible; MSIE 7.0)")
+$wc.Headers.Add("User-Agent", "User-Agent: Mozilla/4.0 (compatible; MSIE 7.0;)")
 
 $result - $wc.DownloadString("https://www.wix.com/")
 $cookie = $wc.ResponseHeaders["Set-Cookie"]
 $cookie = ($cookie.Split(',') -match '^\S+=\S+;' -replace ';.*',") -join '; '
 
 $wc = New-Object System.Net.WebClient
-$wc.Headers.Add("User-Agent", "User-Agent: Mozilla/4.0 (compatible, MSIE 7.0)")
+$wc.Headers.Add("User-Agent", "User-Agent: Mozilla/4.0 (compatible, MSIE 7.0;)")
 $wc.Headers.Add("Cookie", $cookie)
 $postValues = New-Object System.Collections.Specialized.NameValueCollection
 $postValues.Add("email", $credential.GetNetworkCredential().Username)
